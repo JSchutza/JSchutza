8e98d03a1d5e3928e5d08832e23cd74312825912
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-05-31 15:48:23 -0500

    updates to home page

--- frontend/src/components/SkillsViewer/SkillsViewer.js
+++ frontend/src/components/SkillsViewer/SkillsViewer.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container';
-
 import ListGroup from 'react-bootstrap/ListGroup';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -12,32 +14,44 @@ const SkillsViewer = () => {
     <>
     <div>
       <Container >
-      <h1>Skills</h1>
+          <Accordion defaultActiveKey="0">
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="1">
+                <Button variant="primary">
+                  <h1>Skills</h1>
+                </Button>
+              </Accordion.Toggle>
 
-        <ListGroup variant="flush">
-          <ListGroup.Item> JavaScript </ListGroup.Item>
-          <ListGroup.Item> Python </ListGroup.Item>
-          <ListGroup.Item> PHP </ListGroup.Item>
-          <ListGroup.Item> HTML </ListGroup.Item>
-          <ListGroup.Item> CSS </ListGroup.Item>
-          <ListGroup.Item> SQL </ListGroup.Item>
-          <ListGroup.Item> React </ListGroup.Item>
-          <ListGroup.Item> Redux </ListGroup.Item>
-          <ListGroup.Item> Express </ListGroup.Item>
-          <ListGroup.Item> Flask </ListGroup.Item>
-          <ListGroup.Item> Django </ListGroup.Item>
-          <ListGroup.Item> Node.js </ListGroup.Item>
-          <ListGroup.Item> WordPress </ListGroup.Item>
-          <ListGroup.Item> JSON </ListGroup.Item>
-          <ListGroup.Item> Bash Script </ListGroup.Item>
-          <ListGroup.Item> Shell Script </ListGroup.Item>
-          <ListGroup.Item> Git </ListGroup.Item>
-          <ListGroup.Item> Windows </ListGroup.Item>
-          <ListGroup.Item> Linux </ListGroup.Item>
-          <ListGroup.Item> Docker </ListGroup.Item>
-          <ListGroup.Item> Go </ListGroup.Item>
-          <ListGroup.Item> C++ </ListGroup.Item>
-        </ListGroup>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item> JavaScript </ListGroup.Item>
+                    <ListGroup.Item> Python </ListGroup.Item>
+                    <ListGroup.Item> PHP </ListGroup.Item>
+                    <ListGroup.Item> HTML </ListGroup.Item>
+                    <ListGroup.Item> CSS </ListGroup.Item>
+                    <ListGroup.Item> SQL </ListGroup.Item>
+                    <ListGroup.Item> React </ListGroup.Item>
+                    <ListGroup.Item> Redux </ListGroup.Item>
+                    <ListGroup.Item> Express </ListGroup.Item>
+                    <ListGroup.Item> Flask </ListGroup.Item>
+                    <ListGroup.Item> Django </ListGroup.Item>
+                    <ListGroup.Item> Node.js </ListGroup.Item>
+                    <ListGroup.Item> WordPress </ListGroup.Item>
+                    <ListGroup.Item> JSON </ListGroup.Item>
+                    <ListGroup.Item> Bash Script </ListGroup.Item>
+                    <ListGroup.Item> Shell Script </ListGroup.Item>
+                    <ListGroup.Item> Git </ListGroup.Item>
+                    <ListGroup.Item> Windows </ListGroup.Item>
+                    <ListGroup.Item> Linux </ListGroup.Item>
+                    <ListGroup.Item> Docker </ListGroup.Item>
+                    <ListGroup.Item> Go </ListGroup.Item>
+                    <ListGroup.Item> C++ </ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          </Accordion>
       </Container>
     </div>
     </>
