f6b09a3d43ca4e1ef60d915b42555dcc00e433f9
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-24 12:34:38 -0500

    more update to update component

--- frontend/src/components/UpdateInfo/UpdateInfo.js
+++ frontend/src/components/UpdateInfo/UpdateInfo.js
@@ -24,8 +24,32 @@ const UpdateInfo = ({ the_type }) => {
     </div>
 
     <div>
-      
+        {the_type === 'aboutme' ? <div>
+          <h1>Update about me</h1>
+        </div> : <></>}
     </div>
+
+    <div>
+        {the_type === 'skillsviewer' ? <div>
+          <h1>Update skillsviewer</h1>
+        </div> : <></>}
+    </div>
+
+    <div>
+        {the_type === 'projectviewer' ? <div>
+          <h1>Update projectviewer</h1>
+        </div> : <></>}
+    </div>
+
+    <div>
+        {the_type === 'resume' ? <div>
+          <h1>Update resume</h1>
+        </div> : <></>}
+    </div>
+
+
+
+
     </>
   )
 };
