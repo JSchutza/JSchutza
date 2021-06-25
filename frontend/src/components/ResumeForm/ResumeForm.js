a6e093f5bb19d26cb3a80ec0f2282cdc63acf396
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-25 15:36:28 -0500

    started resumeform component

--- frontend/src/components/ResumeForm/ResumeForm.js
+++ frontend/src/components/ResumeForm/ResumeForm.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
 
 
 
 const ResumeForm = () => {
+  const [ skillChoice, setSkillChoice ] = useState(null);
+  const [ projectChoice, setProjectChoice ] = useState(null);
+  const [ experienceChoice, setExperienceChoice ] = useState(null);
+  const [ educationChoice, setEducationChoice ] = useState(null);
+
+
+  const onSubmit = event => {
+    event.preventDefault();
+  }
+
+
+
   return (
     <>
+    <form onSubmit={onSubmit}>
+
+    <label>
+      Confirm Skills
+    </label>
+
+    <label>
+      Confirm Skills
+    </label>
+
+    <label>
+      Confirm Skills
+    </label>
+
+    <label>
+      Confirm Skills
+    </label>
+
+    </form>
     </>
   )
 };
