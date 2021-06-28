cd3d72ad5bde505db7091e751be30ca98f15a82f
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-28 15:38:36 -0500

    seeder updated

--- app/seeds/user.py
+++ app/seeds/user.py
@@ -10,11 +10,11 @@ def seed_user():
   admin = User(
     email=os.environ.get("ADMIN_EMAIL"),
     password=os.environ.get('ADMIN_PASSWORD'),
-    avatar='here',
+    avatar=None,
     firstname='Joshua',
     lastname='Schutza',
-    jobtitle='here',
-    about_text='here',
+    jobtitle='Software Engineer - Leader - Advocate',
+    about_text='I am a determined individual who is working towards a career as a leader, visionary, and advocate. One of my many objectives is to impact the workforce through developing individuals and drawing out their strengths. I am seeking a working and learning opportunity that will utilize my research skills and natural ability to see growth and potential within people and businesses.',
     github_link='https://github.com/JSchutza',
     linkedin_link='https://www.linkedin.com/in/joshua-schutza-559819ba/'
   )
