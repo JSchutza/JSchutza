57a1c211d2f57aa7b3ed0f9691a748fd32bcc1c0
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-28 13:11:27 -0500

    about route done

--- app/api/about_routes.py
+++ app/api/about_routes.py
@@ -3,12 +3,13 @@ from app.models import User
 
 
 
-
-
-
 about_routes = Blueprint('about', __name__)
+
+
+
 
 
 @about_routes.route('/')
 def get_homepage():
-  return { 'message': "sucess" }
+  personal_instance = User.query.get(1)
+  return personal_instance.to_dict()
