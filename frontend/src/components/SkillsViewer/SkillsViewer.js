6d743bf0ce20d1beb01206de45ad4589f20174a3
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-28 16:40:43 -0500

    skillsviewer component updated

--- frontend/src/components/SkillsViewer/SkillsViewer.js
+++ frontend/src/components/SkillsViewer/SkillsViewer.js
@@ -5,12 +5,13 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import { useHistory } from "react-router-dom";
-
+import { useSelector } from 'react-redux';
 
 
 
 const SkillsViewer = ({ isAdmin = false }) => {
   const history = useHistory();
+  const skillInfo = useSelector(store => store.skillsReducer.skills);
 
 
   const handleUpdate = event => {
@@ -39,29 +40,16 @@ const SkillsViewer = ({ isAdmin = false }) => {
               <Accordion.Collapse eventKey="1">
                 <Card.Body>
                   <ListGroup variant="flush">
-                    <ListGroup.Item> JavaScript </ListGroup.Item>
-                    <ListGroup.Item> Python </ListGroup.Item>
-                    <ListGroup.Item> PHP </ListGroup.Item>
-                    <ListGroup.Item> HTML </ListGroup.Item>
-                    <ListGroup.Item> CSS </ListGroup.Item>
-                    <ListGroup.Item> SQL </ListGroup.Item>
-                    <ListGroup.Item> React </ListGroup.Item>
-                    <ListGroup.Item> Redux </ListGroup.Item>
-                    <ListGroup.Item> Express </ListGroup.Item>
-                    <ListGroup.Item> Flask </ListGroup.Item>
-                    <ListGroup.Item> Django </ListGroup.Item>
-                    <ListGroup.Item> Node.js </ListGroup.Item>
-                    <ListGroup.Item> WordPress </ListGroup.Item>
-                    <ListGroup.Item> JSON </ListGroup.Item>
-                    <ListGroup.Item> Bash Script </ListGroup.Item>
-                    <ListGroup.Item> Shell Script </ListGroup.Item>
-                    <ListGroup.Item> Git </ListGroup.Item>
-                    <ListGroup.Item> Windows </ListGroup.Item>
-                    <ListGroup.Item> Linux </ListGroup.Item>
-                    <ListGroup.Item> Docker </ListGroup.Item>
-                    <ListGroup.Item> Go </ListGroup.Item>
-                    <ListGroup.Item> C++ </ListGroup.Item>
-                  </ListGroup>
+                    {skillInfo === null ?
+                        <ListGroup.Item> Loading skills </ListGroup.Item>
+                      :
+                        Object.values(skillInfo).map(eachSkill => (
+                          <>
+                            <ListGroup.Item> {eachSkill.title} </ListGroup.Item>
+                          </>
+                    ))
+                  }
+                </ListGroup>
                 </Card.Body>
               </Accordion.Collapse>
             </Card>
