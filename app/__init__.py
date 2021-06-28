fc6545f848a8c3b297243ef74cfd43119bd5008a
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-28 17:38:52 -0500

    project routes added

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .models import db, User
 from .api.auth_routes import auth_routes
 from .api.about_routes import about_routes
 from .api.skills_routes import skills_routes
+from .api.projects_routes import project_routes
 
 from .seeds import seed_commands
 
@@ -40,6 +41,7 @@ app.config.from_object(Config)
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(about_routes, url_prefix='/api/about')
 app.register_blueprint(skills_routes, url_prefix='/api/skills')
+app.register_blueprint(project_routes, url_prefix='/api/projects')
 
 
 db.init_app(app)
