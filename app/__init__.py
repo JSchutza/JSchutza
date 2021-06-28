68ff12ee29401b1d1dc9891064c72db86c709b59
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-28 15:58:52 -0500

    skills route api started

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .models import db, User
 
 from .api.auth_routes import auth_routes
 from .api.about_routes import about_routes
+from .api.skills_routes import skills_routes
 
 from .seeds import seed_commands
 
@@ -38,6 +39,7 @@ app.config.from_object(Config)
 
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(about_routes, url_prefix='/api/about')
+app.register_blueprint(skills_routes, url_prefix='/api/skills')
 
 
 db.init_app(app)
