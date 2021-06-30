ba22fc284a48a25b49dc060709459096e627221b
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-29 19:04:01 -0500

    EOD stoped on projectviewer component

--- frontend/src/components/ProjectViewer/ProjectViewer.js
+++ frontend/src/components/ProjectViewer/ProjectViewer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import CardColumns from 'react-bootstrap/CardColumns';
 import Card from 'react-bootstrap/Card';
@@ -10,6 +10,19 @@ import { useSelector } from "react-redux";
 const ProjectViewer = ({ isAdmin = false }) => {
   const history = useHistory();
   const projectInfo = useSelector(store => store.projectsReducer.projects);
+  const [isHidden, setIsHidden] = useState(false);
+
+
+
+
+  useEffect(() => {
+    if (!isHidden) {
+      setIsHidden(true);
+    }
+  }, []);
+
+
+
 
 
   const handleUpdate = event => {
@@ -18,166 +31,77 @@ const ProjectViewer = ({ isAdmin = false }) => {
   }
 
 
+  const handleShow = event => {
+    event.preventDefault();
+    setIsHidden(false);
+  }
+
+
+
+  const handleHide = event => {
+    event.preventDefault();
+    setIsHidden(true);
+  }
+
+
+
+
+
   return (
     <>
-      {isAdmin ? <div>
-        <a href="/" onClick={event => handleUpdate(event)}> Update </a>
-      </div> : <></>}
+      {isAdmin ?
+        <Container>
+          <a href="/" onClick={event => handleUpdate(event)}> Update </a>
+        </Container>
+      :
+        <></>
+      }
 
-    <div>
-      <Container>
 
+      {isHidden ?
           <Container>
-            <h1> Projects </h1>
+            <a href="/" onClick={event => handleShow(event)}> <h1> Projects </h1> </a>
           </Container>
+        :
+          <Container>
+
+              <Container>
+                <h1> Projects </h1>
+              </Container>
 
 
-        <CardColumns>
-          {projectInfo === null ?
-              <p></p>
-            :
-              Object.values(projectInfo).map(eachProject => (
-                <>
-              <Card>
-                <Card.Img variant="top" src={defaultImg} />
-                <Card.Body>
-                      <Card.Title> {eachProject.project_name} </Card.Title>
-                  <Card.Text>
-                        {eachProject.description}
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <a href={eachProject.live_link}>
-                    <small className="text-muted">Live</small>
-                  </a>
-                  <br />
+            <CardColumns>
+              {projectInfo === null ?
+                  <p></p>
+                :
+                  Object.values(projectInfo).map(eachProject => (
+                    <>
+                  <Card>
+                    <Card.Img variant="top" src={defaultImg} />
+                    <Card.Body>
+                          <Card.Title> {eachProject.project_name} </Card.Title>
+                      <Card.Text>
+                            {eachProject.description}
+                      </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      <a href={eachProject.live_link}>
+                        <small className="text-muted">Live</small>
+                      </a>
+                      <br />
 
-                  <a href={eachProject.github_link}>
-                    <small className="text-muted">GitHub</small>
-                  </a>
-                </Card.Footer>
-              </Card>
-                </>
-              ))
-          }
-
-
-
-{/*
-          <Card>
-            <Card.Img variant="top" src={defaultImg} />
-            <Card.Body>
-              <Card.Title> Book Organizer </Card.Title>
-              <Card.Text>
-                A app to help authors create and organize books they are currently writing and to connect authors to other aspiring authors.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <a href="https://book-organizer.herokuapp.com/">
-                <small className="text-muted">Live</small>
-              </a>
-                <br />
-              <a href="https://github.com/JSchutza/Book_Organizer">
-                <small className="text-muted">GitHub</small>
-              </a>
-            </Card.Footer>
-          </Card>
-
-          <Card>
-            <Card.Img variant="top" src={defaultImg} />
-            <Card.Body>
-              <Card.Title> Remind Me </Card.Title>
-              <Card.Text>
-                Application to allow developers to efficiently write notes about code.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <a href="https://remind----me.herokuapp.com/">
-                <small className="text-muted">Live</small>
-              </a>
-
-              <br />
-
-              <a href="https://github.com/JSchutza/Remind_Me">
-                <small className="text-muted">GitHub</small>
-              </a>
-            </Card.Footer>
-          </Card>
-
-
-
-          <Card>
-            <Card.Img variant="top" src={defaultImg} />
-            <Card.Body>
-              <Card.Title> Instagram_Clone </Card.Title>
-              <Card.Text>
-                A clone of the Instagram platform, allows users to post photos and connect.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <a href="https://instagram--me.herokuapp.com/">
-                <small className="text-muted">Live</small>
-              </a>
-
-              <br />
-
-              <a href="https://github.com/JSchutza/Instagram_Clone">
-                <small className="text-muted">GitHub</small>
-              </a>
-            </Card.Footer>
-          </Card>
-
-
-
-          <Card>
-            <Card.Img variant="top" src={defaultImg} />
-            <Card.Body>
-              <Card.Title> Podemic </Card.Title>
-              <Card.Text>
-                Application used to discover, review, and save the most recent podcasts.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <a href="https://podemic.herokuapp.com/">
-                <small className="text-muted">Live</small>
-              </a>
-
-              <br />
-
-              <a href="https://github.com/JSchutza/Good_Pods">
-                <small className="text-muted">GitHub</small>
-              </a>
-            </Card.Footer>
-          </Card>
-
-
-
-          <Card>
-            <Card.Img variant="top" src={defaultImg} />
-            <Card.Body>
-              <Card.Title> Median </Card.Title>
-              <Card.Text>
-                A clone of the user driven publishing platform Medium.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <a href="https://median-article-app.herokuapp.com/users">
-                <small className="text-muted">Live</small>
-              </a>
-
-              <br />
-
-              <a href="https://github.com/jxnprince/Median">
-                <small className="text-muted">GitHub</small>
-              </a>
-            </Card.Footer>
-          </Card> */}
-
+                      <a href={eachProject.github_link}>
+                        <small className="text-muted">GitHub</small>
+                      </a>
+                    </Card.Footer>
+                  </Card>
+                    </>
+                  ))
+              }
 
         </CardColumns>
       </Container>
-    </div>
-
+    }
     </>
   )
 };
