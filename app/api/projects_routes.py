7f3cd2058c3c9214ed605eca66a3cc2eef8e7e03
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-07-09 11:20:10 -0500

    update project route done?

--- app/api/projects_routes.py
+++ app/api/projects_routes.py
@@ -95,3 +95,68 @@ def delete_project(project_id):
     return { "message": "project successfully deleted" }
 
   return { "errors": ["Error, cannot remove requested skill.", "Please try again."] }
+
+
+
+
+
+
+
+# PUT /api/projects/:project_id
+@project_routes.route('/<int:project_id>', methods=['PUT'])
+@login_required
+def update_project(project_id):
+  form = UpdateProjectForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+
+  no_errors = True
+
+  project_name = form.data['project_name']
+  img = form.data['img']
+  description = form.data['description']
+  live_link = form.data['live_link']
+  github_link = form.data['github_link']
+  used_tech = form.data['used_tech']
+
+  if project_name == '' or img == '' or description == '' or live_link == '' or github_link == '' or used_tech == '':
+    no_errors = False
+    project_name = 'default project name'
+    img = 'default project img'
+    description = 'default project description'
+    live_link = 'default project live_link'
+    github_link = 'default project github_link'
+    used_tech = 'default project used_tech'
+
+    if form.validate_on_submit():
+      new_project = Project(
+        project_name=project_name,
+        project_img=img,
+        description=description,
+        live_link=live_link,
+        github_link=github_link,
+        used_tech=used_tech
+      )
+
+      db.session.add(new_project)
+      db.session.commit()
+
+      return { new_project.id: new_project.to_dict() }
+
+
+
+  if form.validate_on_submit() and no_errors:
+    new_project = Project(
+      project_name=form.data['project_name'],
+      project_img=form.data['img'],
+      description=form.data['description'],
+      live_link=form.data['live_link'],
+      github_link=form.data['github_link'],
+      used_tech=form.data['used_tech']
+    )
+
+    db.session.add(new_project)
+    db.session.commit()
+
+    return { new_project.id: new_project.to_dict() }
+
+  return { "errors": ["errors", "Please try again."] }
