39541344587d31c13985f675741e0b94433e6170
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-07-13 08:03:09 -0500

    added update education form in flask, started update edu route

--- app/api/education_routes.py
+++ app/api/education_routes.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
-from app.models import Education
+from app.models import db, Education
+from flask_login import login_required
+from app.forms import UpdateEducationForm
+
+
 
 
 educations_routes = Blueprint('educations', __name__)
@@ -10,3 +14,14 @@ def get_homepage():
   educations = Education.query.order_by(Education.start_year).all()
   normalized_data = { each_education.id: each_education.to_dict()  for each_education in educations }
   return { 'educations': normalized_data }
+
+
+
+
+
+
+# POST   /api/educations
+@educations_routes.route('', methods=['POST'])
+@login_required
+def create_new_education():
+  pass
