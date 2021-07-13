a2e0a173708e83dfb765fc308bbb6be0b9726134
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-07-13 12:50:01 -0500

    updates to create experience reducer

--- frontend/src/store/reducers/experiences.js
+++ frontend/src/store/reducers/experiences.js
@@ -9,6 +9,9 @@ const experiencesReducer = (state = { experiences: null, errors: null }, action)
   switch (action.type) {
     case GET_EXPERIENCES:
       return { ...action.experiences };
+    case CREATE_EXPERIENCES:
+      return { experiences: { ...state.experiences, ...action.experiences } };
+
     // case REMOVE_USER:
     //   return { user: null, errors: null };
     // case VALIDATION_ERRORS:
